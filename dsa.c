e2e647f27a7950fcd34c7ac7d91eeba4af4001c7
medinibhat14 <medinibhat.kumta@gmail.com>
2026-03-27 11:10:57 +0530

    Create dsa.c

---
+++ dsa.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+  int a[5]={1,2,3,4,5},i,key=3;
+  for(i=0;i<5;i++){
+    if(a[1]==key){
+      printf("Found");
+      return 0;
+    }
+  else
+      printf("Not found");
+}
+}
